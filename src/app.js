6399def523f899fe2307f17083a0af0dff7517c4
鄭又鉉(ジョンウ・ヒョン) <71251004+whtrade7290@users.noreply.github.com>
2023-07-22 12:16:32 +0900

    Update app.js
    hide mongoDB URI

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ import { authenticateUser } from "./utils/auth.js";
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 mongoose.connect(
-  "mongodb+srv://test:1234@cluster0.xgbluai.mongodb.net/?retryWrites=true&w=majority",
+  "mongodb+srv://id:pw@cluster0.xgbluai.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
